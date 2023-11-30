e9b605a1f9b989d3caec016f1606c80d436f86ef
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-11-30 10:08:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+async function showMessage() {
+  const textInput = document.getElementById("text").value;
+  const delayInput = parseInt(document.getElementById("delay").value, 10);
+
+  // Wait for the specified delay using async/await
+  await new Promise(resolve => setTimeout(resolve, delayInput));
+
+  // Display the text in the output div
+  document.getElementById("output").innerText = textInput;
+}
+
+document.getElementById("btn").addEventListener("click", showMessage);
